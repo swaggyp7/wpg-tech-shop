ed5ca42982008aa08278fce74d2e7edbd8e8ce13
Yuteng Lin <swaggyplyt@gmail.com>
2026-03-24 23:36:33 -0500

    install faker and seed 100 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+category_names = [
+  "Laptops",
+  "Smartphones",
+  "Accessories",
+  "Gaming",
+  "Audio",
+  "Smart Home"
+]
+
+categories = category_names.map do |name|
+  Category.find_or_create_by!(name: name)
+end
+
+Product.delete_all
+Faker::UniqueGenerator.clear
+
+# seed by faker
+100.times do |index|
+  Product.create!(
+    title: "#{Faker::Commerce.product_name} #{index + 1}",
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    price: Faker::Commerce.price(range: 49.0..2999.0),
+    stock_quantity: Faker::Number.between(from: 0, to: 150),
+    category: categories.sample,
+    on_sale: Faker::Boolean.boolean(true_ratio: 25)
+  )
+end
+
+
