a69f27faced40f66cb55195b01c69e0fac64107d
Yuteng Lin <swaggyplyt@gmail.com>
2026-03-24 22:48:17 -0500

    Generate category model and migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_25_034148) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_25_034731) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
