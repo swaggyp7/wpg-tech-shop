243b88f861480e17a0a48405a991507d20e9e723
Yuteng Lin <swaggyplyt@gmail.com>
2026-03-24 22:59:09 -0500

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_25_035044) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_25_035445) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -28,6 +28,16 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_25_035044) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.datetime "order_date"
+    t.decimal "total_price"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -39,5 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_25_035044) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "orders", "customers"
   add_foreign_key "products", "categories"
 end
