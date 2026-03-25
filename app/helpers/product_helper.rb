20714d41f3849e15799094fa2a28f332f5a3f632
Yuteng Lin <swaggyplyt@gmail.com>
2026-03-25 16:12:25 -0500

    fix styling bugs

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -3,6 +3,16 @@ module ProductHelper
     @breadcrumbs.present?
   end
 
+  def discounted_price(product)
+    return product.price unless product.on_sale? && product.discount_percentage.present?
+
+    product.price * (1 - (product.discount_percentage.to_d / 100))
+  end
+
+  def show_discount_price?(product)
+    product.on_sale? && product.discount_percentage.present? && product.discount_percentage.to_d.positive?
+  end
+
   def on_sale_options
     [
       ["All Sale Status", nil],
