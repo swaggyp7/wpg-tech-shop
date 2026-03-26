9f8d98ec402238ceafde9671e7483b8de2f3c5f1
Yuteng Lin <swaggyplyt@gmail.com>
2026-03-25 19:47:27 -0500

    Fix the product relationship and dependent

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many :order_items
+  has_many :cart_items, dependent: :destroy
+  has_many :carts, through: :cart_items
+  has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
 
   has_one_attached :image
@@ -10,6 +12,6 @@ class Product < ApplicationRecord
   end
 
   def self.ransackable_associations(auth_object = nil)
-    ["category", "image_attachment", "image_blob", "order_items", "orders"]
+    ["cart_items", "carts", "category", "image_attachment", "image_blob", "order_items", "orders"]
   end
 end
