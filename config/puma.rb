eb3f9c41179d0eddfea5e585fcb32db2065a120f
Yuteng Lin <swaggyplyt@gmail.com>
2026-04-10 17:39:18 -0500

    fix: crash on runing

--- config/puma.rb
+++ config/puma.rb
@@ -11,10 +11,11 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies that the worker count should equal the number of processors in production.
+# In container platforms, reported CPU counts can be much higher than the
+# resources actually allocated to the service, so default to a single process
+# unless WEB_CONCURRENCY is explicitly set.
 if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY", 1))
   workers worker_count if worker_count > 1
 end
 
